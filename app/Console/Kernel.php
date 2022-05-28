f358b41e1921ab3631023a0e40e0aadd1cf0742e
danibarria <barriareyesjosedaniel@gmail.com>
2022-05-28 17:41:03 -0300

    add command populate:exercises

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        Commands\PopulateExercises::class,
     ];
 
     /**
@@ -25,5 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         //
+        $schedule->command('populate:exercises')
+            ->sendOutputTo('./populate-exercises.log');
+            
     }
 }
